6aa88638022674532017c39018b428a0b94f25f0
Ícaro Baliza <icaroabf2002@gmail.com>
2023-11-07 10:28:52 -0300

    :wrench: configuration: Configurando aparência geral do site

--- simulador-processos-memoria/src/pages/_app.tsx
+++ simulador-processos-memoria/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from '../styles/global'
+import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
