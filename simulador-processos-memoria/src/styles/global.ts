0c78f38c8230380eb0b0d5671a04de3184038634
Ícaro Baliza <icaroabf2002@gmail.com>
2023-11-07 10:22:23 -0300

    :lipstick: feat: Resetando estilização

---
+++ simulador-processos-memoria/src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle };
+
+export const GlobalStyles = styled.createGlobalStyle`
+
+    /* CSS Reset */
+    *, *::after, *::before {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`
