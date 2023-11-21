2b75438c9206472f38ab91c79c3d4eb872189f86
joaoBernardinoo <joaobernardino@ufba.br>
2023-11-21 18:25:56 -0300

    .

--- js/CPU.js
+++ js/CPU.js
@@ -133,3 +133,27 @@ class CPU {
     }
 }
 
+class Process {
+    // Processo com tempo de chegada, tempo de execução, prioridade e deadline
+    constructor({
+      pid = 0,
+      arrival = 0,
+      burst = 99,
+      color = 'white',
+      label = '',
+      deadline = 99,
+      priority = 0
+  
+      // falta deadline e prioridade
+    }) {
+      this.pid = pid;
+      this.arrival = arrival;
+      this.burst = burst;
+      this.color = color;
+      this.label = label;
+      this.scheduled = false;
+      this.deadline = deadline;
+      this.priority = priority
+    }
+  }
+  
