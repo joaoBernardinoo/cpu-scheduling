279581ed7c46581a627bc5c9157aa5a0c7fd5688
Ícaro Baliza <icaroabf2002@gmail.com>
2023-11-21 10:44:45 -0300

    :construction: Desenvolvendo a cpu

--- simulador-processos-memoria/src/Components/cpuComponent/index.tsx
+++ simulador-processos-memoria/src/Components/cpuComponent/index.tsx
@@ -1,48 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 
 import CPU from '@/scripts/cpu';
 import Process from '@/scripts/process';
 
-const tasks = [
-  new Process({
-    pid: 11111,
-    arrival: 0,
-    burst: 8,
-    deadline: 35,
-    label: 'A',
-  }),
-  new Process({
-    pid: 22222,
-    arrival: 4,
-    burst: 4,
-    deadline: 15,
-    label: 'B',
-  }),
-  new Process({
-    pid: 33333,
-    arrival: 8,
-    burst: 2,
-    deadline: 20,
-    label: 'C',
-  }),
-  new Process({
-    pid: 44444,
-    arrival: 12,
-    burst: 7,
-    deadline: 25,
-    label: 'D',
-  }),
-];
+import Modal from '@/Components/Modal';
+
 
 export default function CpuComponent() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const cpu = useState<CPU>(new CPU('FCFS'));
 
-  cpu[0].scheduler.ready = tasks;
+
 
   return (
     <div>
       <h1>CPU</h1>
+      <button onClick={() => cpu[0].run()}>Run</button>
+      <div>
+        <button onClick={() => setModalIsOpen(true)}>Adicionar processo</button>
+      </div>
+      <Modal isOpen={modalIsOpen} setOpenModal={() => setModalIsOpen(!modalIsOpen)} cpu={cpu[0]}/>
     </div>
   )
 }
