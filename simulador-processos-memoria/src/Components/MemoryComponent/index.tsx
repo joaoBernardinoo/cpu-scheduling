0294b3e2df44c6c82ab7ceba56371d23a328eaaa
Caio Viana <caiohebertv@gmail.com>
2023-11-23 21:40:02 -0300

    colcacndo a memória na tela

--- simulador-processos-memoria/src/Components/MemoryComponent/index.tsx
+++ simulador-processos-memoria/src/Components/MemoryComponent/index.tsx
@@ -2,7 +2,7 @@ import RAM from "@/Scripts/memory/RAM"
 import Disk from "@/Scripts/memory/Disk";
 
 export default function MemoryComponent(){
-    const ram = new RAM("LRU");
+    const ram = new RAM();
     const pages = ram.getPages();
 
     const disk = new Disk();
@@ -11,6 +11,17 @@ export default function MemoryComponent(){
     return(
         <div>
             <h2>oiii eu sou a memoria rsrsrs</h2>
+            {/* tira o comentário p fazer a memória aparecer */}
+            {/* {
+                pages.map((pagina, index) => {
+                    return(
+                        <div key={index}>
+                            <p>índice: {index}</p>
+                            <p>ID: {pagina}</p>
+                        </div>
+                    )
+                })
+            } */}
         </div>
     )
 }
