24d83e1fab2e677ec666278058a1f3e0c1603050
Caio Viana <caiohebertv@gmail.com>
2023-11-26 23:30:11 -0300

    selecionar alforitmo

--- simulador-processos-memoria/src/Components/cpuComponent/index.tsx
+++ simulador-processos-memoria/src/Components/cpuComponent/index.tsx
@@ -62,8 +62,10 @@ export default function CpuComponent() {
   const [criteria, setCriteria] = useState<string>('FCFS');
   const [isRunning, setIsRunning] = useState<boolean>(false);
   const cpu = useState<CPU>(new CPU());
-  const [ram, setRam] = useState<RAM>(new RAM());
+  const [selectedOption, setSelectedOption] = useState<string>('FIFO');
+  const [ram, setRam] = useState<RAM>(new RAM(selectedOption));
   const [disk, setDisk] = useState<Disk>(new Disk());
+  
 
   
 
@@ -137,6 +139,11 @@ export default function CpuComponent() {
       }, 1000);
     }
   }
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setSelectedOption(selectedValue);
+  };
 
   /*Executa uma vez a cpu*/
   function handleExec() {
@@ -223,8 +230,12 @@ export default function CpuComponent() {
 
           <SchedulerComponent listStatus={processesStates} listProcess={allProcesses} />
         </SchedulerContainer>
-        {/* <div>
-        </div> */}
+        <div>
+          <select value={selectedOption} onChange={handleSelectChange}>
+            <option value="FIFO">FIFO</option>
+            <option value="LRU">LRU</option>
+          </select>
+        </div>
         <MemoryComponent
          RAM={ram}
          Disk={disk}
